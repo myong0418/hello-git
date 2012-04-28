9d1d54ee74ad1f6e365720f877f347ca8fe13974
myohyeon <gyeong.mh@gmail.com>
2012-04-28 15:38:52 +0900

    init

---
+++ hello-git/src/Gugudan.java
@@ -0,0 +1,30 @@
+
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("input gugudan");
+		Scanner sc = new Scanner(System.in);
+		int i=sc.nextInt();
+		
+		printDan(i);
+		
+		
+//		for (int i = 2; i <= 9; i++) {
+//			printDan(i);
+//		}
+	}
+
+	public static void printDan(int i) {
+		for (int j = 1; j <= 9; j++) {
+			System.out.println(i + "*" + j + "=" + i * j);
+		}
+		System.out.println();
+	}
+
+}
